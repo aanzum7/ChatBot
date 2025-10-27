616c9411d1f2d75ca489b5f7278235f27a0789dc
Tanvir Anzum <50626067+aanzum7@users.noreply.github.com>
2025-10-27 15:21:56 +0600

    Add page icon to Streamlit page configuration

--- pages/Packages.py
+++ pages/Packages.py
@@ -155,7 +155,7 @@ class PackageDisplay:
 
 # ---------- MAIN ----------
 def main():
-    st.set_page_config(page_title="Packages - Rafiya’s Henna Art", layout="wide")
+    st.set_page_config(page_title="Packages - Rafiya’s Henna Art", page_icon="https://lh3.googleusercontent.com/sitesv/AAzXCkf-1N6x6wwyDShvokvM-bQgyx7RMwgRPfrY2rbKk5eVsByjS9PhQ3C0XOd9re_f8VFhc0guPTV7cNUbQclM8N3l3CRpvj2b8Vd_pi89-5OJX0ND_P7HJrplzTH7MOUYdauYisHiQrEbXQjlAOCC9BMeDaifdkSnaTkE0zIotl6eXhHMp7SDjiR95jo=w16383", layout="wide")
     st.markdown(f"""
         <style>
         /* 🌿 Global Page Styling */
